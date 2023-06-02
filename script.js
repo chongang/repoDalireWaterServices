ab41c3e8fd5a426715e2532df7b4e8d52f96aff7
Chong <37767148+chongang@users.noreply.github.com>
2023-06-02 16:18:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,22 @@ $(document).ready(function() {
       email: $('#email').val()
     };
 
-    $.ajax({
-      url: "https://script.google.com/macros/s/AKfycbz9wyOZaUxtE9hJb6tAK3u-TJh1qhp9OBAJBxgklA07DimIw9c-nThr9xIs9uqLDLz1WQ/exec",
-      method: "GET",
-      dataType: "json",
-      data: formData,
-      success: function(response) {
-        alert("Form submitted successfully!");
-        $('#myForm')[0].reset(); // Reset the form after successful submission
-      },
-      error: function(xhr, status, error) {
-        console.error(status + ": " + error);
-        alert("There was an error submitting the form. Please try again.");
-      }
-    });
+    var url = "https://script.google.com/macros/s/AKfycbz9wyOZaUxtE9hJb6tAK3u-TJh1qhp9OBAJBxgklA07DimIw9c-nThr9xIs9uqLDLz1WQ/exec";
+    url += "?name=" + encodeURIComponent(formData.name);
+    url += "&email=" + encodeURIComponent(formData.email);
+    url += "&callback=handleResponse";
+
+    var script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
   });
 });
+
+function handleResponse(response) {
+  if (response.success) {
+    alert("Form submitted successfully!");
+    $('#myForm')[0].reset(); // Reset the form after successful submission
+  } else {
+    console.error("There was an error submitting the form. Please try again.");
+  }
+}
