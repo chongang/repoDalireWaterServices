bcf0c6076a206fc261041cf0e924d9818f26131e
Chong <37767148+chongang@users.noreply.github.com>
2023-06-06 23:56:32 +0800

    Update scriptGetData.js

--- scriptGetData.js
+++ scriptGetData.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-
-});
-
   function getValue() {
-    var id = document.getElementById('accntno').value;
+    var id = $('#accntno').val();
     alert(id);
-    google.script.run.withSuccessHandler(setValue).getAccount(id);
+   var url = "https://script.google.com/macros/s/AKfycbxJyekpyzvZyG_mhp5Rjc2hw_mEauEbpN8GOfgLC70TN74pjJpqFWd-D_U7nfnewu7biA/exec";
+
+    $.ajax({
+      
+      url: url,
+      type: 'POST',
+      data: {
+        id: id
+      },
+      success: function(value) {
+        setValue(value);
+      },
+      error: function(error) {
+        console.log('Error:', error);
+      }
+    });
   }
   
   function setValue(value) {
-    document.getElementById('name').value = value;
+    $('#name').val(value);
   }
+});
