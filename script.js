cc7b200815fb77cfacaec44793317fe140722347
Chong <37767148+chongang@users.noreply.github.com>
2023-06-07 01:53:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -63,6 +63,35 @@ alert("completed document");
   });
 });
 
+ //SET DATA
+  $('#myAccountForm').submit(function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+alert("completed document");
+    var formData = {
+      name: $('#accountno').val(),
+      email: $('#email').val()
+    };
+
+    var url = "https://script.google.com/macros/s/AKfycbxJyekpyzvZyG_mhp5Rjc2hw_mEauEbpN8GOfgLC70TN74pjJpqFWd-D_U7nfnewu7biA/exec";
+    url += "?name=" + encodeURIComponent(formData.name);
+    url += "&email=" + encodeURIComponent(formData.email);
+    
+//alert("completed var params");
+    $.ajax({
+      url: url,
+      dataType: "json",
+      success: function(response) {
+        handleResponse(response);
+        //alert("completed var ajax");
+      },
+      error: function() {
+        console.error("There was an error submitting the form. Please try again.");
+        //alert("fail var ajax");
+      }
+    });
+  });
+});
+
 function handleResponse(response) {
   // Your code here to handle the response
   if (response.success) {
