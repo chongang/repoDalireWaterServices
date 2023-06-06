edf175e15b6ca0d6b66dcd711339ec81a7102678
Chong <37767148+chongang@users.noreply.github.com>
2023-06-06 11:05:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-    function showTab(tabName) {
-      // Hide all tab contents
-      var tabContents = document.getElementsByClassName('tab-content');
-      for (var i = 0; i < tabContents.length; i++) {
-        tabContents[i].style.display = 'none';
-      }
+function showTab(tabName) {
+  // Hide all tab contents
+  var tabContents = document.getElementsByClassName('tab-content');
+  for (var i = 0; i < tabContents.length; i++) {
+    tabContents[i].classList.remove('active');
+  }
 
-      // Show the selected tab content
-      document.getElementById(tabName).style.display = 'block';
+  // Remove 'active' class from all navigation links
+  var tabLinks = document.getElementsByClassName('tab-link');
+  for (var j = 0; j < tabLinks.length; j++) {
+    tabLinks[j].classList.remove('active');
+  }
+
+  // Show the selected tab content and set the navigation link as active
+  document.getElementById(tabName).classList.add('active');
+  event.currentTarget.classList.add('active');
+}
