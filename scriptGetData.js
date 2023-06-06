d44f99c81c161bb137e85ac7247151814115251a
Chong <37767148+chongang@users.noreply.github.com>
2023-06-06 17:06:15 +0800

    Update scriptGetData.js

--- scriptGetData.js
+++ scriptGetData.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   google.script.run.withSuccessHandler(populateInputBox).getDataFromSheet();
 });
 
-function populateInputBox(data) {
+function getAccount(data) {
   var value = data[0][0]; // Adjust the index based on your data structure
   $('#myInput').val(value);
 }
