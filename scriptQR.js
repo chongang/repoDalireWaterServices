a185cd1be6fd90b1a32b40754f93b71c9f29762c
Chong <37767148+chongang@users.noreply.github.com>
2023-06-12 12:47:35 +0800

    Update scriptQR.js

--- scriptQR.js
+++ scriptQR.js
@@ -1,34 +1,40 @@
-// Set up the configuration for QuaggaJS
-const config = {
-  inputStream: {
-    name: "Live",
-    type: "LiveStream",
-    target: document.querySelector("#qrScanner"),
-    constraints: {
-      width: 480,
-      height: 320,
-      facingMode: "environment",
+  // JavaScript
+  // Set up the configuration for QuaggaJS
+  const config = {
+    inputStream: {
+      name: "Live",
+      type: "LiveStream",
+      target: document.querySelector("#qrScanner"),
+      constraints: {
+        width: 480,
+        height: 320,
+        facingMode: "environment",
+      },
     },
-  },
-  decoder: {
-    readers: ["ean_reader"],
-  },
-};
+    decoder: {
+      readers: ["ean_reader"],
+    },
+  };
 
-// Start the scanner
-Quagga.init(config, function (err) {
-  if (err) {
-    console.error(err);
-    return;
+  // Start the scanner
+  function startScanner() {
+    Quagga.init(config, function (err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      Quagga.start();
+    });
   }
-  Quagga.start();
-});
 
-// Listen for a successful scan
-Quagga.onDetected(function (result) {
-  const qrData = result.codeResult.code;
-  document.getElementById("qrDataInput").value = qrData;
+  // Listen for a successful scan
+  Quagga.onDetected(function (result) {
+    const qrData = result.codeResult.code;
+    document.getElementById("qrDataInput").value = qrData;
 
-  // Stop the scanner after a successful scan
-  Quagga.stop();
-});
+    // Stop the scanner after a successful scan
+    Quagga.stop();
+  });
+
+  // Trigger the scanning process when the button is clicked
+  document.getElementById("startScanButton").addEventListener("click", startScanner);
