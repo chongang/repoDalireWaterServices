4024c0e06305b971e3a123f702e069ab410dad3f
Chong <37767148+chongang@users.noreply.github.com>
2023-06-14 15:49:41 +0800

    Update readingAdmin.js

--- readingAdmin.js
+++ readingAdmin.js
@@ -14,3 +14,36 @@
       // Add your desired actions here
     }
   });
+
+
+function loadReadingResult(){
+
+      var data = [
+        { name: "John", age: 25, city: "New York" },
+        { name: "Jane", age: 30, city: "London" },
+        { name: "Bob", age: 35, city: "Paris" }
+      ];
+
+      // Get a reference to the table element
+      var table = document.getElementById("myTable");
+
+      // Generate table header row
+      var header = table.createTHead();
+      var headerRow = header.insertRow();
+      for (var key in data[0]) {
+        var th = document.createElement("th");
+        th.innerHTML = key;
+        headerRow.appendChild(th);
+      }
+
+      // Generate table body rows
+      var body = table.createTBody();
+      for (var i = 0; i < data.length; i++) {
+        var row = body.insertRow();
+        for (var key in data[i]) {
+          var cell = row.insertCell();
+          cell.innerHTML = data[i][key];
+        }
+      }
+
+}
