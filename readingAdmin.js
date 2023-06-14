82590a77223f187124d9b6802cb62121d3b42a5f
Chong <37767148+chongang@users.noreply.github.com>
2023-06-14 15:47:41 +0800

    Update readingAdmin.js

--- readingAdmin.js
+++ readingAdmin.js
@@ -1,1 +1,16 @@
 
+  // Retrieve the checkbox element
+  var checkbox = document.getElementById("myCheckbox");
+
+  // Add an event listener to detect checkbox state changes
+  checkbox.addEventListener("change", function() {
+    if (this.checked) {
+      // Checkbox is checked
+      console.log("Checkbox is checked");
+      // Add your desired actions here
+    } else {
+      // Checkbox is not checked
+      console.log("Checkbox is not checked");
+      // Add your desired actions here
+    }
+  });
