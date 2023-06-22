bee2000702c759698a160d30aa9ee0d344ee0e70
Chong <37767148+chongang@users.noreply.github.com>
2023-06-23 06:57:49 +0800

    Update scriptQR.js

--- scriptQR.js
+++ scriptQR.js
@@ -1,47 +1,24 @@
-// JavaScript
-// Set up the configuration for QuaggaJS
-const config = {
-  inputStream: {
-    name: "Live",
-    type: "LiveStream",
-    target: document.querySelector("#qrScanner"),
-    constraints: {
-      width: 480,
-      height: 320,
-      facingMode: "environment",
-    },
-  },
-  decoder: {
-    readers: ["qrcode_reader"], // Use qrcode_reader decoder for scanning QR codes
-  },
-};
+  // JavaScript
+  const scanner = new Instascan.Scanner({ video: document.getElementById('qrScanner') });
 
-// Start the scanner
-function startScanner() {
-  Quagga.init(config, function (err) {
-    if (err) {
-      console.error("Error initializing Quagga:", err);
-      return;
-    }
-    Quagga.start();
+  // Listen for a successful scan
+  scanner.addListener('scan', function (content) {
+    document.getElementById('qrDataInput').value = content;
+    scanner.stop();
   });
-}
 
-// Listen for a successful scan
-Quagga.onDetected(function (result) {
-  const qrData = result.codeResult.code;
-  document.getElementById("qrDataInput").value = qrData;
+  // Trigger the scanning process when the button is clicked
+  document.getElementById('startScanButton').addEventListener('click', function () {
+    Instascan.Camera.getCameras()
+      .then(function (cameras) {
+        if (cameras.length > 0) {
+          scanner.start(cameras[0]);
+        } else {
+          console.error('No cameras found.');
+        }
+      })
+      .catch(function (error) {
+        console.error('Error accessing cameras:', error);
+      });
+  });
 
-  // Stop the scanner after a successful scan
-  Quagga.stop();
-});
-
-// Listen for errors
-Quagga.onProcessed(function (result) {
-  if (result.error) {
-    console.error("Error processing frame:", result.error);
-  }
-});
-
-// Trigger the scanning process when the button is clicked
-document.getElementById("startScanButton").addEventListener("click", startScanner);
