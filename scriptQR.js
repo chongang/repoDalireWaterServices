f76076c43b00794c7905b43a477ac0a57f2582df
Chong <37767148+chongang@users.noreply.github.com>
2023-06-25 00:56:28 +0800

    Update scriptQR.js

--- scriptQR.js
+++ scriptQR.js
@@ -1,24 +1,28 @@
-  // JavaScript
-  const scanner = new Instascan.Scanner({ video: document.getElementById('qrScanner') });
+      // JavaScript
+      const scanner = new Instascan.Scanner({ video: document.getElementById('qrScanner'), mirror: false });
 
-  // Listen for a successful scan
-  scanner.addListener('scan', function (content) {
-    document.getElementById('qrDataInput').value = content;
-    scanner.stop();
-  });
-
-  // Trigger the scanning process when the button is clicked
-  document.getElementById('startScanButton').addEventListener('click', function () {
-    Instascan.Camera.getCameras()
-      .then(function (cameras) {
-        if (cameras.length > 0) {
-          scanner.start(cameras[0]);
-        } else {
-          console.error('No cameras found.');
-        }
-      })
-      .catch(function (error) {
-        console.error('Error accessing cameras:', error);
+      // Listen for a successful scan
+        scanner.addListener('scan', function (content) {
+        document.getElementById('meternumber').value = content;
+        scanner.stop();
+        hideDWSWindow('QRReaderWindow');
       });
-  });
-
+    
+      // Trigger the scanning process when the button is clicked
+      document.getElementById('startScanButton').addEventListener('click', function () {
+        showDWSWindow('QRReaderWindow');
+        Instascan.Camera.getCameras()
+          .then(function (cameras) {
+            if (cameras.length > 0) {
+              
+              scanner.start(cameras[cameras.length - 1]);
+            } else {
+              console.error('No cameras found.');
+            }
+              scanner.start(activeCamera);
+            
+          })
+          .catch(function (error) {
+            console.error('Error accessing cameras:', error);
+          });
+      });
