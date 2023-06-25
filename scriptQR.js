67321de2ee48c5d7fcbc1eb2debdf32c878e301c
Chong <37767148+chongang@users.noreply.github.com>
2023-06-25 14:39:19 +0800

    Update scriptQR.js

--- scriptQR.js
+++ scriptQR.js
@@ -1,30 +1,10 @@
-      // JavaScript
-      // have the const scanner.. in the hmtl doc, where the qrScanner element is located
-      let scanner = new Instascan.Scanner({ video: document.getElementById('qrScanner'), mirror: false });
-      // Listen for a successful scan
-        scanner.addListener('scan', function (content) {
-        document.getElementById('meternumber').value = content;
-        scanner.stop();
-        hideDWSWindow('QRReaderWindow');
-      });
-    
-      // Trigger the scanning process when the button is clicked
-      document.getElementById('startScanButton').addEventListener('click', function () {
-      scanner = new Instascan.Scanner({ video: document.getElementById('qrScanner'), mirror: false });
 
-        showDWSWindow('QRReaderWindow');
-        Instascan.Camera.getCameras()
-          .then(function (cameras) {
-            if (cameras.length > 0) {
-              
-              scanner.start(cameras[cameras.length - 1]);
-            } else {
-              console.error('No cameras found.');
-            }
-             // scanner.start(activeCamera);
-            
-          })
-          .catch(function (error) {
-            console.error('Error accessing cameras:', error);
-          });
-      });
+function validateDWSM(content){
+  if (content.startsWith("DWSM:")) {
+    // Extract the remaining text after ":"
+    var remainingText = content.substring(5);
+    return remainingText;
+  } else {
+    return "invalid";
+  }
+}
